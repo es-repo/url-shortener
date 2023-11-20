8ad6383e13a59a96f14e09a6a0854805ee3a6263
MacBook Pro 14 <macbookpro14@MacBooks-MBP.fritz.box>
2023-11-21 01:05:28 +0200

    chore: Take port from env.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import express from 'express';
 import decodeRouter from './routes/decode';
 import encodeRouter from './routes/encode';
 
-// TODO: Move to config file
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
